ab963652eabc39ddba057d6ec803572f0e6dbdac
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2016-02-29 19:31:07 -0600

    T4Resolve is working away from NIL machines.

--- src/+mlio/LogParser.m
+++ src/+mlio/LogParser.m
@@ -40,7 +40,78 @@ classdef LogParser < mlio.AbstractParser
         end
     end
     
-	methods 	
+	methods  		
+        function [contnt,idx] = findNextCell(this, fieldName, idx0)
+            assert(ischar(fieldName));
+            assert(isnumeric(idx0));
+            contnt = [];
+            idx = idx0;
+            for c = idx0:length(this.cellContents_) 
+                if (lstrfind(this.cellContents_{c}, fieldName))
+                    contnt = this.cellContents_{c};
+                    idx = c;
+                    break
+                end
+            end
+            if (isempty(contnt))
+                error('mlio:endOfFile', 'LogParser.findNextCell found nothing more'); end
+        end	
+        function [contntCells,idx] = findNextNCells(this, fieldName, idx0, N)
+            assert(ischar(fieldName));
+            assert(isnumeric(idx0));
+            assert(isnumeric(N));
+            contntCells = cell(1,N);
+            idx = idx0;
+            for c = idx0:length(this.cellContents_) 
+                if (lstrfind(this.cellContents_{c}, fieldName))
+                    for n = 1:N
+                        contntCells{n} = this.cellContents_{c+n-1};
+                    end
+                    idx = c+N-1;
+                    break
+                end
+            end
+            if (isempty(contntCells))
+                error('mlio:endOfFile', 'LogParser.findNextNCells found nothing more'); end
+        end	
+        function [nvs,idx1] = nextLineNNumeric(this, fieldName, varargin)
+            p = inputParser;
+            addRequired(p, 'fieldName', @ischar);
+            addOptional(p, 'N',    1,   @isnumeric);
+            addOptional(p, 'idx0', 1,   @isnumeric);
+            parse(p, fieldName, varargin{:});
+            
+            [lines,idx1] = this.findNextNCells(p.Results.fieldName, p.Results.idx0, 2);
+            if (isempty(lines) || isempty(lines{2}))
+                nvs = []; 
+                return
+            end
+            frmt = '\\s*(?<value1>%s)';
+            spec = {this.ENG_PATT_LOW};
+            for n = 2:p.Results.N
+                frmt = [frmt '\\s+(?<value' num2str(n) '>%s)'];
+                spec = [spec {this.ENG_PATT_LOW}];
+            end
+            args  = [{frmt} spec];
+            names = regexp(lines{2}, sprintf(args{:}), 'names');
+            for n = 1:p.Results.N
+                nvs(1,n) = str2double(strtrim(names.(['value' num2str(n)])));
+            end
+        end
+        function [nv,idx1] = rightSideChar(this, fieldName, varargin)
+            p = inputParser;
+            addRequired(p, 'fieldName', @ischar);
+            addOptional(p, 'idx0', 1,   @isnumeric);
+            parse(p, fieldName, varargin{:});
+            
+            [line,idx1] = this.findNextCell(fieldName, p.Results.idx0);
+            if (isempty(line))
+                nv = []; 
+                return
+            end
+            names = regexp(line, sprintf('%s\\s*(?<value1>\\S+)', fieldName), 'names');
+            nv    = strtrim(names.value1);
+        end
         function [nv,idx1] = rightSideNumeric(this, fieldName, varargin)
             p = inputParser;
             addRequired(p, 'fieldName', @ischar);
@@ -53,7 +124,7 @@ classdef LogParser < mlio.AbstractParser
                 return
             end
             names = regexp(line, sprintf('%s\\s+(?<value1>%s)', fieldName, this.ENG_PATT_LOW), 'names');
-            nv = str2num(strtrim(names.value1)); %#ok<ST2NM>
+            nv    = str2num(strtrim(names.value1)); %#ok<ST2NM>
         end
         function [nv,idx1] = rightSideNumeric2(this, fieldName, varargin)
             p = inputParser;
@@ -70,21 +141,7 @@ classdef LogParser < mlio.AbstractParser
             names = regexp(line, ...
                     sprintf('%s\\s+(?<value1>%s)\\s+%s\\s+(?<value2>%s)', ...
                             p.Results.fieldName, this.ENG_PATT_LOW, p.Results.fieldName2, this.ENG_PATT_LOW), 'names');
-            nv = str2num(strtrim([names.value1 ' ' names.value2])); %#ok<ST2NM>
-        end
- 		
-        function [contnt,idx] = findNextCell(this, fieldName, idx0)
-            contnt = [];
-            idx = idx0;
-            for c = idx0:length(this.cellContents_) 
-                if (lstrfind(this.cellContents_{c}, fieldName))
-                    contnt = this.cellContents_{c};
-                    idx = c;
-                    break
-                end
-            end
-            if (isempty(contnt))
-                throw(MException('mlio:endOfFile', 'LogParser.findNextCell found nothing more')); end
+            nv    = str2num(strtrim([names.value1 ' ' names.value2])); %#ok<ST2NM>
         end
     end 
     
---
+++ test/+mlio_unittest/Test_LogParser.m
@@ -0,0 +1,95 @@
+classdef Test_LogParser < matlab.unittest.TestCase
+	%% TEST_LOGPARSER 
+
+	%  Usage:  >> results = run(mlio_unittest.Test_LogParser)
+ 	%          >> result  = run(mlio_unittest.Test_LogParser, 'test_dt')
+ 	%  See also:  file:///Applications/Developer/MATLAB_R2014b.app/help/matlab/matlab-unit-test-framework.html
+
+	%  $Revision$
+ 	%  was created 26-Feb-2016 17:15:15
+ 	%  by jjlee,
+ 	%  last modified $LastChangedDate$
+ 	%  and checked into repository /Users/jjlee/Local/src/mlcvl/mlio/test/+mlio_unittest.
+ 	%% It was developed on Matlab 9.0.0.307022 (R2016a) Prerelease for MACI64.
+ 	
+
+	properties
+ 		registry
+ 		testObj
+ 	end
+
+	methods (Test)
+		function test_rightSideNumeric(this)
+ 			[nv,idx1] = this.testObj.rightSideNumeric('eta,q', 1);
+            this.verifyEqual(nv, 0.43829);
+            this.verifyEqual(idx1, 30);
+ 		end
+		function test_nextLineNNumeric(this)
+            [nvs,idx1] = this.testObj.nextLineNNumeric('parameters', 6, 30);
+            this.verifyEqual(nvs, [0.2506    0.0266    0.0712   -0.2442    0.2305   -0.2641]);
+            this.verifyEqual(idx1, 33);
+            [nvs,idx2] = this.testObj.nextLineNNumeric('100000*second partial in parameter space', 6, idx1);
+            this.verifyEqual(nvs, [745.      721.      956.      448.      518.      333.]);
+            this.verifyEqual(idx2, 90);
+        end
+        function test_rightSideChar(this)
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 1);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame4_b55.4dfp.img');
+            this.verifyEqual(idx1, 6);    
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 7);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame5_b55.4dfp.img');
+            this.verifyEqual(idx1, 19);
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 20);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame4_b55.4dfp.img');
+            this.verifyEqual(idx1, 105);    
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 106);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame5_b55.4dfp.img');
+            this.verifyEqual(idx1, 118);
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 119);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame4_b55.4dfp.img');
+            this.verifyEqual(idx1, 169);
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 170);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame6_b55.4dfp.img');
+            this.verifyEqual(idx1, 182);
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 183);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame4_b55.4dfp.img');
+            this.verifyEqual(idx1, 268);
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 269);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame6_b55.4dfp.img');
+            this.verifyEqual(idx1, 281);
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 282);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame4_b55.4dfp.img');
+            this.verifyEqual(idx1, 332);
+            [nv,idx1] = this.testObj.rightSideChar('Reading image:', 333);
+            this.verifyEqual(nv, 'NP995_09fdg_v1_frame7_b55.4dfp.img');
+            this.verifyEqual(idx1, 345);
+        end
+	end
+
+ 	methods (TestClassSetup)
+		function setupLogParser(this)
+ 			import mlio.*;
+ 			this.testObj_ = LogParser.load( ...
+                fullfile('/Users/jjlee/Local/src/mlcvl/mlio/test/+mlio_unittest', 'imgreg_4dfp.log'));
+ 		end
+	end
+
+ 	methods (TestMethodSetup)
+		function setupLogParserTest(this)
+ 			this.testObj = this.testObj_;
+ 			this.addTeardown(@this.cleanFiles);
+ 		end
+	end
+
+	properties (Access = private)
+ 		testObj_
+ 	end
+
+	methods (Access = private)
+		function cleanFiles(this)
+ 		end
+	end
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
